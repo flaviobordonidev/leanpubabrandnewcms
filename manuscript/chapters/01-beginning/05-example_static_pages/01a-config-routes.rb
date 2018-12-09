b649fc5ea20aff542dc60cf3d239428de7db4136
flavio bordoni dev <flavio.bordoni.dev@gmail.com>
2018-12-09 22:17:32 +0000

    Add chapters

--- manuscript/chapters/01-beginning/05-example_static_pages/01a-config-routes.rb
+++ manuscript/chapters/01-beginning/05-example_static_pages/01a-config-routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'test_pages#page_a'
-  get 'test_pages/page_a'
-  get 'test_pages/page_b'
-
+  root 'example_static_pages#page_a'
+  
+  get 'example_static_pages/page_a'
+  get 'example_static_pages/page_b'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
