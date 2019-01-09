adf3c58ddd546c2d9eab7dd6c3b02e7dbf749b86
flavio bordoni dev <flavio.bordoni.dev@gmail.com>
2019-01-09 23:17:56 +0000

    Add chap authorization-users

--- manuscript/chapters/01-beginning/09-authorization/03_02-db-schema.rb
+++ manuscript/chapters/01-beginning/09-authorization/03_02-db-schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_27_232018) do
+ActiveRecord::Schema.define(version: 2019_01_08_110724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2018_12_27_232018) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
