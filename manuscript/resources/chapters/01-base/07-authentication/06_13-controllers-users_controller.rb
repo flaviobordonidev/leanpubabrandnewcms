7bc72935e18784ab87c44383c0a76036678ef021
flavio bordoni dev <flavio.bordoni.dev@gmail.com>
2019-11-11 14:27:59 +0000

    chap 7.6

--- manuscript/resources/chapters/01-base/07-authentication/06_13-controllers-users_controller.rb
+++ manuscript/resources/chapters/01-base/07-authentication/06_13-controllers-users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_created_at)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
 end
